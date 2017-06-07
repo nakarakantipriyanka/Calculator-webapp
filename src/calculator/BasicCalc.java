bc8e37e72390a6f587763c93451ed21df8598265
HOME <HOME@192.168.0.13>
2017-06-07 11:16:08 +0530

    BasicCalc program code by-N.priyanka

--- src/calculator/BasicCalc.java
+++ src/calculator/BasicCalc.java
@@ -3,10 +3,28 @@ package calculator;
 
 public class BasicCalc
 {
-	// include variables a, b, c here
-	 
-	//include getters and setters method to access variables
+	double a,b,c;
+	public double getA() {
+		return a;}
+	public void setA(double a) {
+		this.a = a;}
+	public double getB() {
+		return b;}
+	public void setB(double b) {
+		this.b = b;}
+
+
+			double Add(){
+				c=a+b;
+				return(c);}
+			double Subtract(){
+				c=a-b;
+				return(c);}
+			double Multiply(){
+				c=a*b;
+				return(c);}
+			double Divide(){
+				c=a/b;
+				return(c);}
+	}
 	
-	//include methods for operations
-	
-}
--- src/calculator/Servletcontroller.java
+++ src/calculator/Servletcontroller.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
+
 @WebServlet("/ServletController")
 
 
@@ -26,30 +27,48 @@ import javax.servlet.http.HttpSession;
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException 
 	{
-		// Create an object of BasicCalc class
+        BasicCalc det= new BasicCalc();
+        
+        double a =Double.valueOf(req.getParameter("a"));
+        double b =Double.valueOf(req.getParameter("b"));
+        det.setA(a);
+        det.setB(b);
+       String operation=req.getParameter("Operation");
+	
 		
-		//get parameter with req.getparameter() method and 
-		
-		// set the values with set parameter into variable a, b
-		
-		// get parameter operation 
-		
-	//	switch (operation)		
+
+		switch (operation)		
 		{
-		//write switch cases for calling different method of operations
+		case "Add":
+		c=det.Add();
+		System.out.println("c");
+		break;
+		case "Subtract":
+		c=det.Subtract();
+		System.out.println("c");
+		break;
+		case "Multiply":
+		c=det.Multiply();
+		System.out.println("c");
+		break;
+		case "Divide":
+		c=det.Divide();
+		System.out.println("c");
+		break;
+		default:System.out.println("wrong choice");
+		break;
 		}
-		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
-		rd.forward(req, resp); 
-		} 
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException {
-	doGet(req, resp);
+          
+		req.setAttribute("answer",c);
+		RequestDispatcher rd=req.getRequestDispatcher("index.jsp");
+		rd.forward(req,resp);
 	}
-}
-	
+	@Override
+	protected void doPost(HttpServletRequest req,HttpServletResponse resp)
+	throws ServletException,IOException
+	{
+		doGet(req,resp);
+	}
 
-
-
-	
+}	
+		
